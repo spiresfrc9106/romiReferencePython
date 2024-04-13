ef17ce559d2fab51ac405596ee3e0a8bb420fd03
spiresFrc9106 <ccrobotics@grcatholiccentral.org>
2024-04-13 16:46:27 -0400

    Adding source code
    Signed-off-by: spiresFrc9106 <ccrobotics@grcatholiccentral.org>

---
+++ robot.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
+
+#
+# Example that shows how to connect to a ROMI from RobotPy
+#
+# Requirements
+# ------------
+#
+#    # Install https://github.com/wpilibsuite/WPILibPi/releases/download/v2023.2.1/WPILibPi_64_image-v2023.2.1-Romi.zip
+#    # on your Raspberry Pi sd card.
+#
+#    # On Windows, some people prefer to run python 3
+#    py -3
+#
+#    # but sometimes when using Python virtual environments (venv) "py -3" does not run the python associated with
+#    # the virtual environment, some people (this author) avoids "py -3", preferring "python"
+#
+#    # confirm that your python is 3.12 or greater
+#    python -VV
+#
+#    python -m pip install robotpy
+#    python -m pip install robotpy-halsim-ws
+#
+#
+# Run the program
+# ---------------
+#
+# To run the program you will need to explicitly use the ws-client option:
+#
+#    cd to this directory
+#    python -m robotpy sync
+#
+#    power-up the Romi
+#    connect to a WiFi network where the romi is on.
+#
+#    python -m robotpy sim --wc-client
+#
+# By default the WPILib simulation GUI will be displayed. To disable the display
+# you can add the --nogui option
+#
+
+import os
+import typing
+
+import romi
+import wpilib
+import commands2
+
+from robotcontainer import RobotContainer
+
+# Uncomment these lines and set the port to the pycharm debugger to use the
+# Pycharm debug server to debug this code.
+
+#import pydevd_pycharm
+#pydevd_pycharm.settrace('localhost', port=61890, stdoutToServer=True, stderrToServer=True)
+
+# If your ROMI isn't at the default address, set that here
+os.environ["HALSIMWS_HOST"] = "10.0.0.2"
+os.environ["HALSIMWS_PORT"] = "3300"
+
+
+class MyRobot(commands2.TimedCommandRobot):
+    """
+    Command v2 robots are encouraged to inherit from TimedCommandRobot, which
+    has an implementation of robotPeriodic which runs the scheduler for you
+    """
+
+    autonomousCommand: typing.Optional[commands2.Command] = None
+
+    def robotInit(self) -> None:
+        """
+        This function is run when the robot is first started up and should be used for any
+        initialization code.
+        """
+
+        # Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        # autonomous chooser on the dashboard.
+        self.container = RobotContainer()
+
+    def disabledInit(self) -> None:
+        """This function is called once each time the robot enters Disabled mode."""
+
+    def disabledPeriodic(self) -> None:
+        """This function is called periodically when disabled"""
+
+    def autonomousInit(self) -> None:
+        """This autonomous runs the autonomous command selected by your RobotContainer class."""
+        self.autonomousCommand = self.container.getAutonomousCommand()
+
+        if self.autonomousCommand:
+            self.autonomousCommand.schedule()
+
+    def autonomousPeriodic(self) -> None:
+        """This function is called periodically during autonomous"""
+
+    def teleopInit(self) -> None:
+        # This makes sure that the autonomous stops running when
+        # teleop starts running. If you want the autonomous to
+        # continue until interrupted by another command, remove
+        # this line or comment it out.
+        if self.autonomousCommand:
+            self.autonomousCommand.cancel()
+
+    def teleopPeriodic(self) -> None:
+        """This function is called periodically during operator control"""
+
+    def testInit(self) -> None:
+        # Cancels all running commands at the start of test mode
+        commands2.CommandScheduler.getInstance().cancelAll()
